de0869a0cd4cce87a786ef289452e526d27ddc14
KimsyTor <88418792+KimsyTor@users.noreply.github.com>
2024-02-08 18:55:19 +0700

    Add files via upload

--- Python codes/Chapter 1/challenge1.py
+++ Python codes/Chapter 1/challenge1.py
@@ -13,3 +13,36 @@ print(b) # 10
 
 a = 10
 b = 20
+
+#1st solution
+
+##a = a+ b # a=30
+##b = a- b # b= 10
+##a = a-b # a=20
+##
+##print(a)
+##print(b)
+##
+###2nd solution
+##
+##c = (a+b)/2 #c=15
+##d = (a-b)/2 #d=-5
+##a = c-d # a = 20
+##b = c+d # b=10
+##print(a)
+##print(b)
+
+#3rd solution
+a,b = b,a
+print(a)
+print(b)
+
+#4th solution
+
+c = a #c=10
+a = b #a=20
+b = c #b= 10
+
+print(a)
+print(b)
+
