c25f7e6624462d5c581b8c552bd4132073954b95
KimsyTor <88418792+KimsyTor@users.noreply.github.com>
2024-02-13 19:14:51 +0700

    Add files via upload

--- Python codes/Chapter 2/challenge.py
+++ Python codes/Chapter 2/challenge.py
@@ -1,15 +1,26 @@
 '''
-We have 2 variables, a and b, and we want to swap their values.
-variable a = 10 and variable b = 20
 
-rules:
-1. variable a and b can't be directly assgined an integer 
-    example: a = 20, and b = 10
+Challenge: Write a function "letter_grade" that take 1 parameter call "grade_point."
 
-result:
-print(a) # 20
-print(b) # 10
+This function will return a letter grade based AUPP's grading system.
+
+See here: https://www.aupp.edu.kh/grading-grade-point-average-grade-change/
+
+A	93-100%	    4.00	Outstanding attainment of course goals
+A-	90-92.99%	3.67	Superior attainment of course goals
+B+	87-89.99%	3.33	Very good attainment of course goals
+B	83-86.99%	3.00	Good attainment of course goals
+B-	80-82.99%	2.67	Well above average attainment of course goals
+C+	77-79.99%	2.33	Above average attainment of course goals
+C	73-76.99%	2.00	Average attainment of course goals
+C-	70-72.99%	1.67	Below average attainment of course goals.
+D+	67-69.99%	1.33	Weak attainment of course goals
+D	63-66.99%	1.00	Poor attainment of course goals
+D-	60-62.99%	0.67	Very poor attainment of course goals
+F	below 60%	0.00	Unsatisfactory attainment of course goals
+
 '''
 
-a = 10
-b = 20
+def letter_grade(grade_point):
+    # Add your code here
+    pass
